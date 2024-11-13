d6c0610937ea3be7357e05ede160942c17e446fb
osamakalam <abhdul2004@gmail.com>
2024-11-13 09:46:15 +0530

    updates

---
+++ Projects/src/project2/customer/CustomerExecuter.java
@@ -0,0 +1,48 @@
+package project2.customer;
+
+public class CustomerExecuter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Customer c1 = new Customer();
+		Customer c2 = new Customer();
+		Customer c3 = new Customer();
+		Customer c4 = new Customer();
+		Customer c5 = new Customer();
+		
+		c1.setCid(4004);
+		c1.setCname("abhdulkalam");
+		c1.setAddress("3/792, muthumala rediyar nagar");
+		c1.setCity("villupuram");
+		
+		c2.setCid(4005);
+		c2.setCname("dhineshkumar");
+		c2.setAddress("gedilam 24 near kari kadai");
+		c2.setCity("panrutti");
+		
+		c3.setCid(4006);
+		c3.setCname("harish");
+		c3.setAddress("maanguppam");
+		c3.setCity("villupuram");
+		
+		c4.setCid(4007);
+		c4.setCname("bala");
+		c4.setAddress(" insane nagar ");
+		c4.setCity("chidamabram");
+		
+		c5.setCid(4017);
+		c5.setCname("chanrabalan");
+		c5.setAddress("  3 is a govemnt ");
+		c5.setCity(" villupram ");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		
+
+	}
+
+}
